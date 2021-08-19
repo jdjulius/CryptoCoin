3eda4bdf4e2f19e5b33f6a8c737eb66bad2a3679
Julius-central <julio.david.velasquez@gmail.com>
2021-08-18 22:57:00 -0600

    Finalizacion de proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,89 @@
 <template>
-  <h1>HELLOW WORD</h1>
+  <div class="container">
+    <h1>Coin Market</h1>
+    <input
+      type="text"
+      class="form-control bg-dark text-light rounded-0 border-0 my-4"
+      placeholder="Search Coin"
+      @keyup="searchCoin()"
+      v-model="textSearch"
+    />
+    <table class="table table-dark">
+      <thead>
+        <tr>
+          <td v-for="title in titles" :key="title">
+            {{ title }}
+          </td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(coin, index) in filteredCoins" :key="coin.id">
+          <td class="text-muted">
+            {{ index + 1 }}
+          </td>
+          <td>
+            <img :src="coin.image" style="width: 2rem" class="me-2" />
+            <span>
+              {{ coin.name }}
+            </span>
+            <span class="ms-2 text-uppercase text-muted">
+              {{ coin.symbol }}
+            </span>
+          </td>
+          <td>${{ coin.current_price }}</td>
+          <td
+            :class="[
+              coin.price_change_percentage_24h > 0
+                ? 'text-success'
+                : 'text-danger',
+            ]"
+          >
+            {{ coin.price_change_percentage_24h }} %
+          </td>
+          <td>${{ coin.total_volume.toLocaleString() }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-  mounted:{
-    
-  }
+import axios from "axios";
 
-}
+export default {
+  name: "App",
+  data() {
+    return {
+      coins: [],
+      filteredCoins: [],
+      titles: ["#", "Coin", "Price", "Prince Change", "24 Volume"],
+      textSearch: "",
+    };
+  },
+  async mounted() {
+    await axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((response) => {
+        console.log(response);
+        this.coins = response.data;
+        this.filteredCoins = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  methods: {
+    searchCoin() {
+      this.filteredCoins = this.coins.filter((coin) =>
+        coin.name.toLowerCase().includes(this.textSearch.toLowerCase())  || 
+        coin.symbol.toLowerCase().includes(this.textSearch.toLowerCase())  
+      );
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
